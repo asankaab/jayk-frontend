02d0b5179500a1425b15854130f92e4f8ae0baeb
asanka <asanka.abewickrama@gmail.com>
2024-11-27 14:04:27 +0530

    fixes

--- src/components/FavouriteButton.jsx
+++ src/components/FavouriteButton.jsx
@@ -1,13 +1,17 @@
 'use client'
 import { useState } from "react";
 import { Button } from "./ui/button"
-import { Bookmark } from "lucide-react";
+import { Bookmark, BookmarkCheck } from "lucide-react";
+import { save } from "@/actions/strapiApi";
 
-export default function FavouriteButton({disabled, isFav}) {
+export default function FavouriteButton({disabled, isFav, productId}) {
 
     const [ state, setState ] = useState(isFav);
     
     return (
-        <Button size="lg" onClick={() => setState(!state)} disabled={disabled} type="submit" className={state ? "bg-secondary text-white" : "text-secondary bg-transparent border border-secondary hover:bg-secondary hover:text-white disabled:border-neutral-200 disabled:text-neutral-200 disabled:hover:bg-transparent"}><div className=" flex items-center gap-2 ">Save <Bookmark size={18}/></div></Button>
+        <Button onClick={async() => {
+            setState(!state)
+            await save(productId);
+        }} disabled={disabled} type="submit" className={"text-secondary bg-transparent border border-secondary hover:bg-secondary hover:text-white disabled:border-neutral-200 disabled:text-neutral-200 disabled:hover:bg-transparent"}><div className=" flex items-center gap-1 "><Bookmark fill={state ? "rgb(215 18 67)" : "none"}/></div></Button>
     )
 }
